2d4e6ca567cd944c59b57635b7d8af726b315090
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-09-15 17:19:12 +0100

    added import button in Recipes.tsx

--- frontend/src/Pages/Recipes/Recipes.tsx
+++ frontend/src/Pages/Recipes/Recipes.tsx
@@ -1,11 +1,16 @@
+import { Button } from "react-bootstrap";
 import SearchBar from "../../Components/SearchBar/SearchBar";
+import "./Recipes.scss";
 
-const Recipes:React.FC = () => {
+const Recipes: React.FC = () => {
     return (
-    <>
-    <h1>Recipes</h1>
-    <SearchBar />
-    </>
+        <>
+            <h1>Recipes</h1>
+            <SearchBar />
+            <div className="button-box">
+                <Button className="custom-button" size="lg">Import Recipe from Website</Button>
+            </div>
+        </>
     );
 }
 
