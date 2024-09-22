0e17e73bebd7ae5dd2bc8058d6a3cc15917911bf
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-09-22 15:30:18 +0100

    separated recipe form components

--- frontend/src/Components/RecipeForm/RecipeForm.tsx
+++ frontend/src/Components/RecipeForm/RecipeForm.tsx
@@ -1,22 +1,10 @@
-import { Button, Form } from "react-bootstrap";
-
-// import RecipeSummary from "../RecipeSummary/RecipeSummary";
+import { Form } from "react-bootstrap";
+import RecipeSummary from "../RecipeSummary/RecipeSummary";
 
 const RecipeForm: React.FC = () => {
     return (
         <Form>
-            <Form.Group className="mb-3" controlId="recipe-name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control className="custom-form-control" type="text" placeholder="Enter Recipe Name" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="recipe-description">
-                <Form.Label>Description</Form.Label>
-                <Form.Control className="custom-form-control" type="text" placeholder="Description" />
-            </Form.Group>
-            <Button className="mb-3 custom-button" size="lg" style={{ fontSize: '1rem' }} type="button">
-                Add Photo
-            </Button>
+            <RecipeSummary />
         </Form>
     )
 
--- frontend/src/Components/RecipeSummary/RecipeSummary.tsx
+++ frontend/src/Components/RecipeSummary/RecipeSummary.tsx
@@ -4,7 +4,18 @@ import "./RecipeSummary.scss";
 const RecipeSummary: React.FC = () => {
     return (
         <>
+            <Form.Group className="mb-3" controlId="recipe-name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control className="custom-form-control" type="text" placeholder="Enter Recipe Name" />
+            </Form.Group>
 
+            <Form.Group className="mb-3" controlId="recipe-description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control className="custom-form-control" type="text" placeholder="Description" />
+            </Form.Group>
+            <Button className="mb-3 custom-button" size="lg" style={{ fontSize: '1rem' }} type="button">
+                Add Photo
+            </Button>
 
         </>
     )
