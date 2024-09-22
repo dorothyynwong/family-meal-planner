d0beeb57b3c7eebc6d2cbcda88fde8f94393949f
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-09-22 16:01:30 +0100

    added button to add rows, need to work with the layout

--- frontend/src/Components/RecipeIngredient/RecipeIngredient.tsx
+++ frontend/src/Components/RecipeIngredient/RecipeIngredient.tsx
@@ -1,25 +1,52 @@
 import { useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Form, FormLabel, Row } from "react-bootstrap";
+
 
 const RecipeIngredient: React.FC = () => {
     const [rowCount, setRowCount] = useState(5);
 
+    const handleClick = () => {
+        setRowCount(rowCount+1);
+    }
+
     return (
-        <>
+        <Form.Group className="mb-3" controlId="ingredients-list">
+            <FormLabel>Ingredients</FormLabel>
+            <Row key="ingredients-header">
+                <Col key="quantity-header" style={{ textAlign: "center", fontWeight: "bold"}}>Quantity</Col>
+                <Col key="unit-header" style={{ textAlign: "center", fontWeight: "bold"}}>Unit</Col>
+                <Col key="ingredient-header" style={{ textAlign: "center", fontWeight: "bold"}}>Ingredient</Col>
+            </Row>
             {Array.from({ length: rowCount }, (_, i) => (
                 <Row key={i}>
-                    <Col key={i-1}>
-                    {i}-1
+                    <Col key={i - 1}>
+                        <Form.Control
+                            className="mb-3 custom-form-control"
+                            type="text"
+                            id={`quantity-${i}`}
+                            aria-describedby="quantity"
+                        />
                     </Col>
-                    <Col key={i-2}>
-                    {i}-2
+                    <Col key={i - 2}>
+                        <Form.Control
+                            className="mb-3 custom-form-control"
+                            type="text"
+                            id={`unit-${i}`}
+                            aria-describedby="unit"
+                        />
                     </Col>
-                    <Col key={i-3}>
-                    {i}-3
+                    <Col key={i - 3}>
+                        <Form.Control
+                            className="mb-3 custom-form-control"
+                            type="text"
+                            id={`ingredient--${i}`}
+                            aria-describedby="ingredient"
+                        />
                     </Col>
                 </Row>
             ))}
-        </>
+            <Button onClick={handleClick}>Add More Ingredients</Button>
+        </Form.Group>
     )
 }
 
