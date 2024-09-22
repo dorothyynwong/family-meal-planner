cd91b37cba3a7f1a088c3a956b5535b2157daf9d
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-09-22 13:15:06 +0100

    change gitignore

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85282feae145946bcc93e3b0ae89fbd26f84cbf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ac90146e8cb3c0381b97d85b847d1d55b0747f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
