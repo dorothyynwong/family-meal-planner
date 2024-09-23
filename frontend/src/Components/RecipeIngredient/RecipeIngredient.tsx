994bb66823aa2a413f8fa334999a43985e12881c
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-09-23 11:07:57 +0100

    format ingredients to form

--- frontend/src/Components/RecipeIngredient/RecipeIngredient.tsx
+++ frontend/src/Components/RecipeIngredient/RecipeIngredient.tsx
@@ -2,10 +2,32 @@ import { useState } from "react";
 import { Button, Col, Form, FormLabel, Row } from "react-bootstrap";
 import { ImportRecipeInterface } from "../../Api/apiInterface";
 
+interface Ingredient {
+    quantity: string;
+    name: string;
+  }
+
+  const parseIngredient = (ingredient: string): Ingredient => {
+
+    const regex = /^(\S+)\s+(.+)$/; 
+    const match = ingredient.match(regex);
+  
+    if (match) {
+      const isQuantity = /^\d/.test(match[1]); 
+      return {
+        quantity: isQuantity ? match[1].trim() : '', 
+        name: isQuantity ? match[2].trim() : match[0].trim() 
+      };
+    }
+  
+    return { quantity: '', name: ingredient }; 
+  };
 
 const RecipeIngredient: React.FC<ImportRecipeInterface> = (data) => {
-    const ingredients = data.recipeIngredients;
-    const [rowCount, setRowCount] = useState(ingredients && ingredients.length > 0 ? ingredients.length : 5);
+    const ingredientsList = data.recipeIngredients;
+    const parsedIngredients = ingredientsList?.map(parseIngredient);
+    
+    const [rowCount, setRowCount] = useState(ingredientsList && ingredientsList.length > 0 ? ingredientsList.length : 5);
 
     const handleClick = () => {
         setRowCount(rowCount+1);
@@ -15,36 +37,28 @@ const RecipeIngredient: React.FC<ImportRecipeInterface> = (data) => {
         <Form.Group className="mb-3" controlId="ingredients-list">
             <FormLabel>Ingredients</FormLabel>
             <Row key="ingredients-header">
-                <Col key="quantity-header" style={{ textAlign: "center", fontWeight: "bold"}}>Quantity</Col>
-                <Col key="unit-header" style={{ textAlign: "center", fontWeight: "bold"}}>Unit</Col>
-                <Col key="ingredient-header" style={{ textAlign: "center", fontWeight: "bold"}}>Ingredient</Col>
+                <Col key="quantity-header" style={{ textAlign: "center", fontWeight: "bold"}} xs={4}>Quantity</Col>
+                <Col key="ingredient-header" style={{ textAlign: "center", fontWeight: "bold"}} xs={8}>Ingredient</Col>
             </Row>
             {Array.from({ length: rowCount }, (_, i) => (
                 <Row key={i+1}>
-                    <Col key={`col-1`}>
+                    <Col key={`col-1`} xs={4}>
                         <Form.Control
                             className="mb-3 custom-form-control"
                             type="text"
                             aria-label={`quantity-${i+1}`}
                             aria-describedby="quantity"
+                            value={parsedIngredients? parsedIngredients[i].quantity : ""}
                             
                         />
                     </Col>
-                    <Col key={`col-2`}>
-                        <Form.Control
-                            className="mb-3 custom-form-control"
-                            type="text"
-                            aria-label={`unit-${i+1}`}
-                            aria-describedby="unit"
-                        />
-                    </Col>
-                    <Col key={`col-3`}>
+                    <Col key={`col-2`} xs={8}>
                         <Form.Control
                             className="mb-3 custom-form-control"
                             type="text"
                             aria-label={`ingredient-${i+1}`}
                             aria-describedby="ingredient"
-                            value={data.recipeIngredients? data.recipeIngredients[i] : ""}
+                            value={parsedIngredients? parsedIngredients[i].name : ""}
                         />
                     </Col>
                 </Row>
