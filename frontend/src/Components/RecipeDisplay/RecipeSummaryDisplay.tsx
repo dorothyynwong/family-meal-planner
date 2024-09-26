84a4a62511ba9bbad0856de3a42196e5035823a0
Dorothy Wong <dorothyynwong@gmail.com>
2024-09-26 12:27:07 +0100

    trying listgroup

--- frontend/src/Components/RecipeDisplay/RecipeSummaryDisplay.tsx
+++ frontend/src/Components/RecipeDisplay/RecipeSummaryDisplay.tsx
@@ -1,9 +1,21 @@
+import { ListGroup } from "react-bootstrap";
 import { RecipeDetailsProps } from "./RecipeDisplay";
 
 const RecipeSummaryDisplay: React.FC<RecipeDetailsProps> = ({data}) => {
    
     return (
-        <>{data?.name}</>
+<ListGroup className="mb-3">
+    <ListGroup.Item>
+        <strong>Name:</strong> {data?.name}
+    </ListGroup.Item>
+    <ListGroup.Item>
+        <strong>Description:</strong> {data?.description}
+    </ListGroup.Item>
+    <ListGroup.Item>
+        <strong>Notes:</strong> {data?.notes}
+    </ListGroup.Item>
+</ListGroup>
+
     )
 
 }
