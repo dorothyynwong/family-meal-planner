4d581040ddfd2e52c79a6ca019f28d85da9d99e9
Dorothy Wong <dorothyynwong@gmail.com>
2024-09-27 10:21:07 +0100

    fixed navigation links

--- frontend/src/Components/Navigation/Navigation.tsx
+++ frontend/src/Components/Navigation/Navigation.tsx
@@ -9,10 +9,10 @@ const Navigation: React.FC = () => {
                 <img src={logo} alt="Family Meal Planner" style={{ height: '100px' }} />
                 </Navbar.Brand>
                 <Nav className="custom-navbar-links">
-                    <Nav.Link className="custom-nav-link" href="home">Home</Nav.Link>
-                    <Nav.Link className="custom-nav-link" href="recipes">Recipes</Nav.Link>
-                    <Nav.Link className="custom-nav-link" href="meals">Meals</Nav.Link>
-                    <Nav.Link className="custom-nav-link" href="shopping-list">Shopping List</Nav.Link>
+                    <Nav.Link className="custom-nav-link" href="/home">Home</Nav.Link>
+                    <Nav.Link className="custom-nav-link" href="/recipes">Recipes</Nav.Link>
+                    <Nav.Link className="custom-nav-link" href="/meals">Meals</Nav.Link>
+                    <Nav.Link className="custom-nav-link" href="/shopping-list">Shopping List</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
