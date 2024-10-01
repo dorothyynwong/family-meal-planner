0318213744302ea25dea56ee99b9a8668b1514b9
Dorothy Wong <dorothyynwong@gmail.com>
2024-10-01 10:56:32 +0100

    added empty mealplanmonthly and mealplancard

---
+++ frontend/src/Components/MealPlanCard/MealPlanCard.tsx
@@ -0,0 +1,5 @@
+const MealPlanCard:React.FC = () => {
+    return(<>Meal Plan Card</>)
+}
+
+export default MealPlanCard;
---
+++ frontend/src/Pages/MeaPlanlMonthly/MealPlanMonthly.tsx
@@ -0,0 +1,14 @@
+import MealPlanCalendar from "../../Components/MealPlanCalendar/MealPlanCalendar"
+import MealPlanCard from "../../Components/MealPlanCard/MealPlanCard"
+
+const MealPlanMonthly:React.FC = () => {
+    return (
+        <>
+                <MealPlanCalendar />
+                <MealPlanCard />
+        </>
+
+    )
+}
+
+export default MealPlanMonthly
