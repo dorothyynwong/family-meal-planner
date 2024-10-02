62e666b9cd933067e60a400786b991134aeabd21
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-02 17:44:35 +0100

    added meal cards with different colors for different meal types

--- frontend/src/Components/MealCard/MealCard.tsx
+++ frontend/src/Components/MealCard/MealCard.tsx
@@ -1,11 +1,19 @@
 import { MealDetailsInterface } from "../../Api/apiInterface";
+import { Card, CardContent, CardHeader } from "@mui/material";
 
 interface MealProps {
     meal: MealDetailsInterface
 }
 
-const MealCard:React.FC<MealProps> = ({meal}) => {
-    return(<>{meal.id}</>)
+const MealCard: React.FC<MealProps> = ({ meal }) => {
+    return (
+        <Card sx={{ maxWidth: 345, m:2 }}>
+            <CardHeader title={meal.mealType} className={meal.mealType} />
+            <CardContent>
+                {meal.name}
+            </CardContent>
+        </Card>
+    )
 }
 
 export default MealCard;
