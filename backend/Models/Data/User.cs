9f20d5cbadff6215614708a427273b31a243be49
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-06 09:34:20 +0100

    added models

--- backend/Models/Data/User.cs
+++ backend/Models/Data/User.cs
@@ -4,5 +4,8 @@ namespace FamilyMealPlanner.Models.Data;
 
 public class User : IdentityUser<int>
 {
-    
+    public string? FirstName { get; set; }
+    public string? LastName { get; set;}
+
+    public ICollection<FamilyUser> FamilyUsers { get; set; }
 }
