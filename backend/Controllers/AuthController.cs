d7bdc9628530b194c1b002060d679da06715e312
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-07 12:37:51 +0100

    returns jwt after login

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -1,11 +1,14 @@
 
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 using FamilyMealPlanner.Enums;
 using FamilyMealPlanner.Models;
 using FamilyMealPlanner.Models.Data;
 using FamilyMealPlanner.Models.Request;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using NLog;
 
 namespace FamilyMealPlanner.Controllers;
@@ -19,44 +22,60 @@ public class AuthController(UserManager<User> userManager, RoleManager<Role> rol
     private readonly RoleManager<Role> _roleManager = roleManager;
     NLog.ILogger Logger = LogManager.GetCurrentClassLogger();
 
-    // [HttpPost("login")]
-    // public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
-    // {
-    //     var matchingUser = await _userManager.FindByNameAsync(loginRequest.UserName);
-    //     if (matchingUser != null && await _userManager.CheckPasswordAsync(matchingUser, loginRequest.Password))
-    //     {
-    //         var matchingUserRoles = await _userManager.GetRolesAsync(matchingUser);
-    //         var authClaims = new List<Claim>
-    //         {
-    //             new(ClaimTypes.NameIdentifier, matchingUser.Id.ToString()),
-    //             new(ClaimTypes.Name, matchingUser.UserName!),
-    //             new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-    //         };
-    //         foreach (var role in matchingUserRoles)
-    //         {
-    //             authClaims.Add(new Claim(ClaimTypes.Role, role));
-    //         }
-    //         var token = new JwtSecurityToken(
-    //             issuer: _configuration["Jwt:Issuer"],
-    //             audience: _configuration["Jwt:Audience"],
-    //             expires: DateTime.UtcNow.AddHours(1),
-    //             claims: authClaims,
-    //             signingCredentials: new SigningCredentials(
-    //                 new SymmetricSecurityKey(Encoding.Default.GetBytes(_configuration["Jwt:Secret"]!)),
-    //                 SecurityAlgorithms.HmacSha256
-    //             )
-    //         );
-    //         return Ok(
-    //             new TokenResponse
-    //             {
-    //                 Token = new JwtSecurityTokenHandler().WriteToken(token),
-    //                 Expiration = token.ValidTo,
-    //                 RoleType = token.Claims.ElementAt(3).Value
-    //             }
-    //         );
-    //     }
-    //     return Unauthorized();
-    // }
+    private static SigningCredentials CreateSigningCredentials()
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("VerySecretAndLongKey-NeedMoreSymbolsHere-123")), 
+            SecurityAlgorithms.HmacSha256);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] UserLoginRequest loginRequest)
+    {
+        var matchingUser = await _userManager.FindByNameAsync(loginRequest.UserName);
+        if (matchingUser != null && await _userManager.CheckPasswordAsync(matchingUser, loginRequest.Password))
+        {
+            var matchingUserRoles = await _userManager.GetRolesAsync(matchingUser);
+            var authClaims = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, matchingUser.Id.ToString()),
+                new(ClaimTypes.Name, matchingUser.UserName!),
+                // new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+            foreach (var role in matchingUserRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var jwt = new JwtSecurityToken(
+                        issuer: "issuer",
+                        audience: "audience",
+                        claims: authClaims,
+                        expires: DateTime.UtcNow.AddHours(1),
+                        signingCredentials: CreateSigningCredentials()
+                        );
+            // var token = new JwtSecurityToken(
+            //     issuer: _configuration["Jwt:Issuer"],
+            //     audience: _configuration["Jwt:Audience"],
+            //     expires: DateTime.UtcNow.AddHours(1),
+            //     claims: authClaims,
+            //     signingCredentials: new SigningCredentials(
+            //         new SymmetricSecurityKey(Encoding.Default.GetBytes(_configuration["Jwt:Secret"]!)),
+            //         SecurityAlgorithms.HmacSha256
+            //     )
+            // );
+            return Ok(
+            // new TokenResponse
+            // {
+            //     Token = new JwtSecurityTokenHandler().WriteToken(token),
+            //     Expiration = token.ValidTo,
+            //     RoleType = token.Claims.ElementAt(3).Value
+            // }
+            new JwtSecurityTokenHandler().WriteToken(jwt)
+            );
+        }
+        return Unauthorized();
+    }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] UserRegisterRequest userRequest)
