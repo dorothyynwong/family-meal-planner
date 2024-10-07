f8d8ca330a9abd3bec0a3e9e05c34e01044a1e0b
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-07 08:36:22 +0100

    added guid to family

---
+++ backend/Migrations/20241007073546_AddGuidToFamily.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGuidToFamily : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EncryptedGuid",
+                table: "Families",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncryptedGuid",
+                table: "Families");
+        }
+    }
+}
