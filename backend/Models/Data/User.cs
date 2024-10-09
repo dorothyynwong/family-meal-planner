d6916ae14898ea053f1475a2b2867f0c2380780d
Dorothy Wong <dorothyynwong@gmail.com>
2024-10-09 12:24:47 +0100

    changed Email as required in user table

--- backend/Models/Data/User.cs
+++ backend/Models/Data/User.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace FamilyMealPlanner.Models.Data;
 
 public class User : IdentityUser<int>
 {
+    [Required]
+    public string Email {get; set;}
     public string? Nickname { get; set; }
 
     public ICollection<FamilyUser> FamilyUsers { get; set; }
