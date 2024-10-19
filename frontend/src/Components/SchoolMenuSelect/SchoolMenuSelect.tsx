d8f429cc4ca1a979f2e1306efc9b7c2f4e655fea
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-19 17:51:28 +0100

    not show schoolmenuselect if no schoolmeals

--- frontend/src/Components/SchoolMenuSelect/SchoolMenuSelect.tsx
+++ frontend/src/Components/SchoolMenuSelect/SchoolMenuSelect.tsx
@@ -3,34 +3,44 @@ import { SchoolMealInterface } from "../../Api/apiInterface";
 import { useMeal } from "../MealContext/MealContext";
 import { useEffect, useState } from "react";
 import { getSchoolMealsByDate } from "../../Api/api";
+import StatusHandler from "../StatusHandler/StatusHandler";
 
 interface SchoolMenuSelectProps {
 }
 
-const SchoolMenuSelect: React.FC<SchoolMenuSelectProps> = ({}) => {
-    const {mealDate} = useMeal();
+const SchoolMenuSelect: React.FC<SchoolMenuSelectProps> = ({ }) => {
+    const { mealDate } = useMeal();
     const [status, setStatus] = useState<"idle" | "loading" | "success" | "error">("idle");
     const [errorMessages, setErrorMessages] = useState<string[]>([]);
-    const [schoolMeals, setSchoolMeals] = useState<SchoolMealInterface[]>();
+    const [schoolMeals, setSchoolMeals] = useState<SchoolMealInterface[]>([]);
 
-    useEffect(() => 
-    {
+    useEffect(() => {
+        setSchoolMeals([]);
         getSchoolMealsByDate(6, mealDate)
-        .then(response => {
-            setStatus("loading");
-            setSchoolMeals(response.data);
-        })
-        .catch(error => {
-            setSchoolMeals([]);
-            console.log("Error getting school meals", error);
-            const errorMessage = error?.response?.data?.message || "Error getting school meals";
-            setStatus("error");
-            setErrorMessages([...errorMessages, errorMessage]);
-        });
-    } ,[mealDate]);
+            .then(response => {
+                setStatus("loading");
+                setSchoolMeals(response.data);
+                setStatus("success");
+            })
+            .catch(error => {
+                setSchoolMeals([]);
+                console.log("Error getting school meals", error);
+                const errorMessage = error?.response?.data?.message || "Error getting school meals";
+                setStatus("error");
+                setErrorMessages([...errorMessages, errorMessage]);
+            });
+    }, [mealDate]);
 
     return (<>
-        {schoolMeals && <Form.Select aria-label="Default select example">
+        <StatusHandler
+            status={status}
+            errorMessages={errorMessages}
+            loadingMessage="getting school meals ..."
+            successMessage=""
+        >
+            <></>
+        </StatusHandler>
+        {schoolMeals && schoolMeals.length >0 && <Form.Select aria-label="Default select example">
             <option>Select a Meal</option>
             {schoolMeals.map((meal, index) => <option key={index} value={meal.id}>{meal.mealName} - {meal.category}</option>)}
         </Form.Select>}
