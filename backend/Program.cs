ca57573ef40755fe52955772278882495d579f53
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-23 17:29:34 +0100

    changed program.cs to add environment variable to configurationbuilder

--- backend/Program.cs
+++ backend/Program.cs
@@ -14,6 +14,11 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configurationBuilder = new ConfigurationBuilder()
+    .AddEnvironmentVariables(); 
+
+var configuration = configurationBuilder.Build();
+
 NLog.ILogger Logger = LogManager.GetCurrentClassLogger();
 
 string currentDirectory = System.IO.Directory.GetCurrentDirectory();
