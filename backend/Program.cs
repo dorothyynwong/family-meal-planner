becef2329320921a7a0219236383ea534fd8afc9
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-27 08:44:34 +0000

    try hardcode orgin

--- backend/Program.cs
+++ backend/Program.cs
@@ -89,7 +89,8 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins(builder.Configuration["Cors:Frontend"]!).AllowAnyMethod()
+        // policy.WithOrigins(builder.Configuration["Cors:Frontend"]!).AllowAnyMethod()
+        policy.WithOrigins("https://nimble-semolina-9b1e47.netlify.app/").AllowAnyMethod()
                                                                     .AllowAnyHeader()
                                                                     .AllowCredentials();
     });
