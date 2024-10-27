49a0c0d26d9a51ef37214a36606fca7bbbcfa33a
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-27 09:28:48 +0000

    revert to use appsettings.json for cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -89,12 +89,9 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        // policy.WithOrigins(builder.Configuration["Cors:Frontend"]!).AllowAnyMethod()
-        policy.WithOrigins("https://nimble-semolina-9b1e47.netlify.app").AllowAnyMethod()
+        policy.WithOrigins(builder.Configuration["Cors:Frontend"]!).AllowAnyMethod()
                                                                     .AllowAnyHeader()
                                                                     .AllowCredentials();
-//policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
-
     });
 });
 
