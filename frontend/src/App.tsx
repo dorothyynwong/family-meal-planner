b50e0df4e8b75a5598d4247c63b50ebd2c895c03
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-29 15:24:22 +0000

    moved mui custom theme to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ import { createTheme, ThemeProvider } from '@mui/material';
 
 const theme = createTheme({
   typography: {
-    fontFamily: "Fredoka", 
+    fontFamily: "Fredoka",
   },
   palette: {
     primary: {
@@ -41,59 +41,59 @@ function App() {
     <AuthProvider>
       <MealProvider>
         <ThemeProvider theme={theme}>
-        <Router>
-          <Layout>
-            <Routes>
-              <Route path="/login"
-                element={<UserLoginPage />} />
+          <Router>
+            <Layout>
+              <Routes>
+                <Route path="/login"
+                  element={<UserLoginPage />} />
 
-              <Route path="/signup"
-                element={<UserSignupPage />} />
+                <Route path="/signup"
+                  element={<UserSignupPage />} />
 
 
-              <Route element={<PrivateRoute />}>
-                <Route path="/"
-                  element={<Home />} />
+                <Route element={<PrivateRoute />}>
+                  <Route path="/"
+                    element={<Home />} />
 
-                <Route path="/home"
-                  element={<Home />} />
-                <Route path="/recipes"
-                  element={<Recipes />} />
+                  <Route path="/home"
+                    element={<Home />} />
+                  <Route path="/recipes"
+                    element={<Recipes />} />
 
-                <Route path="/recipes-list/"
-                  element={<RecipesList />} />
+                  <Route path="/recipes-list/"
+                    element={<RecipesList />} />
 
-                <Route path="/recipe-add/:recipeId"
-                  element={<RecipeCreation />} />
+                  <Route path="/recipe-add/:recipeId"
+                    element={<RecipeCreation />} />
 
-                <Route path="/recipe-add"
-                  element={<RecipeCreation />} />
+                  <Route path="/recipe-add"
+                    element={<RecipeCreation />} />
 
-                <Route path="/recipe-details/:recipeId"
-                  element={<RecipeDetails />} />
+                  <Route path="/recipe-details/:recipeId"
+                    element={<RecipeDetails />} />
 
-                <Route path="/recipe-edit/:recipeId"
-                  element={<RecipeUpdate />} />
+                  <Route path="/recipe-edit/:recipeId"
+                    element={<RecipeUpdate />} />
 
-                <Route path="/meal-plans"
-                  element={<MealPlanMonthly />} />
+                  <Route path="/meal-plans"
+                    element={<MealPlanMonthly />} />
 
-                <Route path="/families"
-                  element={<Families />} />
+                  <Route path="/families"
+                    element={<Families />} />
 
-                <Route path="/my-families"
-                  element={<FamiliesList data={[]} />} />
+                  <Route path="/my-families"
+                    element={<FamiliesList data={[]} />} />
 
-                <Route path="/family-meals"
-                  element={<FamilyMealDaily />} />
+                  <Route path="/family-meals"
+                    element={<FamilyMealDaily />} />
 
-                <Route path="/school-menu-edit"
-                  element={<SchoolMenuEdit />} />
-              </Route>
+                  <Route path="/school-menu-edit"
+                    element={<SchoolMenuEdit />} />
+                </Route>
 
-            </Routes>
-          </Layout>
-        </Router>
+              </Routes>
+            </Layout>
+          </Router>
         </ThemeProvider>
       </MealProvider>
     </AuthProvider>
