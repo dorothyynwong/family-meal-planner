81abc6a1151cd0b492fe4b33c25cbb1ad4648e94
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-10-30 08:43:40 +0000

    added icon to edit meal on mealcard

--- frontend/src/Components/MealCard/MealCard.tsx
+++ frontend/src/Components/MealCard/MealCard.tsx
@@ -1,8 +1,7 @@
 import { MealDetailsInterface } from "../../Api/apiInterface";
 import { Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { useMeal } from "../MealContext/MealContext";
-// import OverflowMenu from "../OverflowMenu/OverflowMenu";
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { FaEdit } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 interface MealProps {
@@ -13,11 +12,6 @@ interface MealProps {
 const MealCard: React.FC<MealProps> = ({ meal, isReadOnly }) => {
     const { setModalShow, setMealDate, setMealNotes, setSelectedMealType, setCurrentMeal, setRecipeName, setMode } = useMeal();
     const navigate = useNavigate();
-
-    // const menuItems = [
-    //     { id: "display-recipe-button", label: "Recipe Details" },
-    //     { id: "edit-meal-button", label: "Edit Meal" }
-    // ];
 
     const handleClick = () => {
         if (!isReadOnly) {
@@ -35,33 +29,12 @@ const MealCard: React.FC<MealProps> = ({ meal, isReadOnly }) => {
        navigate(`/recipe-details/${meal.recipeId}`);
     }
 
-    // const handleOptionsClick = (option: string) => {
-    //     switch (option) {
-    //         case "display-recipe-button":
-    //             navigate(`/recipe-details/${meal.recipeId}`);
-    //             break
-    //         case "edit-meal-button":
-    //             if (!isReadOnly) {
-    //                 setModalShow(true);
-    //                 setMode("Edit");
-    //                 setCurrentMeal(meal);
-    //                 setMealDate(meal.date);
-    //                 setSelectedMealType(meal.mealType);
-    //                 setMealNotes(meal.notes ? meal.notes : "");
-    //                 setRecipeName(meal.recipeName ? meal.recipeName : "");
-    //             }
-    //             break
-    //         default:
-    //             break
-    //     }
-    // }
-
     return (
         <Card sx={{ maxWidth: 345, mx: 0, mb: 1 }}>
             <CardHeader
                 title={meal.mealType}
                 className={meal.mealType}
-                // action={<OverflowMenu menuItems={menuItems} handleOptionsClick={handleOptionsClick} icon={MoreVertIcon} />}
+                action={<FaEdit onClick={handleClick}/>}
             />
             {(meal.recipeDefaultImage) && <CardMedia
                 component="img"
