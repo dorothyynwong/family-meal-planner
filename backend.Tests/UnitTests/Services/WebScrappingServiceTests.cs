2aa26a63a112d58ba45266c14c89db798fcaf077
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-11-01 13:34:54 +0000

    changed unittests for added recipe field

--- backend.Tests/UnitTests/Services/WebScrappingServiceTests.cs
+++ backend.Tests/UnitTests/Services/WebScrappingServiceTests.cs
@@ -1,64 +1,67 @@
-// using FamilyMealPlanner.Models;
-// using FamilyMealPlanner.Services;
-// using FluentAssertions;
+using FamilyMealPlanner.Models;
+using FamilyMealPlanner.Services;
+using FluentAssertions;
 
-// [TestFixture]
-// public class WebScrappingServiceTests
-// {
-//     private WebScrappingService _service = new WebScrappingService();
+[TestFixture]
+public class WebScrappingServiceTests
+{
+    private WebScrappingService _service = new WebScrappingService();
 
-//     [SetUp]
-//     public void Setup()
-//     {
-//     }
+    [SetUp]
+    public void Setup()
+    {
+    }
 
-//     [Test]
-//     public async Task ImportRecipe_ValidUrl_ReturnsRecipe()
-//     {
-//         var url = "https://www.bbc.co.uk/food/recipes/air_fryer_chicken_27325";
+    [Test]
+    public async Task ImportRecipe_ValidUrl_ReturnsRecipe()
+    {
+        var url = "https://www.allrecipes.com/recipe/45396/easy-pancakes/";
 
-//         Recipe recipe = new Recipe();
-//         recipe.Name = "Air fryer sticky chicken drumsticks";
-//         recipe.Images = [
-//                         "https://food-images.files.bbci.co.uk/food/recipes/air_fryer_chicken_27325_16x9.jpg"
-//                         ];
-//         recipe.Description =  "This sticky marinade is a winner – super quick to make and adds a wonderfully sweet and rich flavour to chicken pieces.";
-//         recipe.RecipeIngredients = [
-//                                     "2 tbsp light soy sauce",
-//                                     "3 tbsp tomato chutney (or any type will work)",
-//                                     "2 tbsp runny honey",
-//                                     "1 tsp Worcestershire sauce",
-//                                     "8 chicken legs (see recipe tip)",
-//                                     "potato salad and green crunchy leaves",
-//                                   ];
-//         recipe.RecipeInstructions = [
-//                                     "In a large bowl, mix together the marinade ingredients, then add the chicken and coat well with the marinade. Cover and keep in the fridge for anything from 20 minutes up to 24 hours.",
-//                                     "When ready to cook, preheat the air fryer to 180C.",
-//                                     "Lay the drumsticks in a single layer in the basket and cook for 10 minutes, then turn over and cook for another 10–12 minutes until they are crispy and charred.",
-//                                     "Serve with a potato salad and green crunchy leaves.",
-//                                     ];
-//         recipe.DefaultImageUrl = "https://food-images.files.bbci.co.uk/food/recipes/air_fryer_chicken_27325_16x9.jpg";
+        Recipe recipe = new Recipe();
+        recipe.Name = "Easy Pancakes";
+        recipe.Images = [
+                        "https://www.allrecipes.com/thmb/pZtxkWhiaZdUmdhgv-Pj9EIMbVY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/45396-easy-pancakes-DDMFS-4x3-44411f993d7841d9b2b89bcc65bdd178.jpg"
+                        ];
+        recipe.Description =  "This easy pancake recipe is quick to make with simple pantry ingredients. They cook up nice and fluffy for a family-pleasing breakfast or brunch.";
+        recipe.RecipeIngredients = [
+                                    "1 cup all-purpose flour",
+                                    "2 tablespoons white sugar",
+                                    "2 teaspoons baking powder",
+                                    "1 teaspoon salt, or to taste",
+                                    "1 cup milk",
+                                    "2 tablespoons vegetable oil",
+                                    "1 egg, beaten"
+                                  ];
+        recipe.RecipeInstructions = [
+                                    "Gather the ingredients.",
+                                    "Combine flour, sugar, baking powder, and salt in a large bowl. Make a well in the center, and pour in milk, oil, and egg. Mix until smooth.",
+                                    "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop batter onto the griddle, using approximately 1/4 cup for each pancake; cook until bubbles form and the edges are dry, 1 to 2 minutes. Flip and cook until browned on the other side. Repeat with remaining batter.",
+                                    "Serve hot and enjoy!",
+                                    ];
+        recipe.DefaultImageUrl = "https://www.allrecipes.com/thmb/pZtxkWhiaZdUmdhgv-Pj9EIMbVY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/45396-easy-pancakes-DDMFS-4x3-44411f993d7841d9b2b89bcc65bdd178.jpg";
+        recipe.RecipeUrl = url;
 
 
-//         var result = await _service.GetRecipeFromUrl(url);
-//         Assert.That(result, Is.Not.Null);
+        var result = await _service.GetRecipeFromUrl(url);
+        Assert.That(result, Is.Not.Null);
 
-//         result.Name.Should().BeEquivalentTo(recipe.Name);
-//         result.Images.Should().BeEquivalentTo(recipe.Images);
-//         result.Description.Should().BeEquivalentTo(recipe.Description);
-//         result.RecipeIngredients.Should().BeEquivalentTo(recipe.RecipeIngredients);
-//         result.RecipeInstructions.Should().BeEquivalentTo(recipe.RecipeInstructions);
-//         result.DefaultImageUrl.Should().BeEquivalentTo(recipe.DefaultImageUrl);
-//     }
+        result.Name.Should().BeEquivalentTo(recipe.Name);
+        result.Images.Should().BeEquivalentTo(recipe.Images);
+        result.Description.Should().BeEquivalentTo(recipe.Description);
+        result.RecipeIngredients.Should().BeEquivalentTo(recipe.RecipeIngredients);
+        result.RecipeInstructions.Should().BeEquivalentTo(recipe.RecipeInstructions);
+        result.DefaultImageUrl.Should().BeEquivalentTo(recipe.DefaultImageUrl);
+        result.RecipeUrl.Should().BeEquivalentTo(recipe.RecipeUrl);
+    }
 
-//     [Test]
-//     public void ImportRecipe_InValidUrl_ReturnsRecipe()
-//     {
-//         var url = "http://google.com";
+    [Test]
+    public void ImportRecipe_InValidUrl_ReturnsRecipe()
+    {
+        var url = "http://google.com";
         
-//         var exception = Assert.ThrowsAsync<Exception>(async () =>
-//         await _service.GetRecipeFromUrl(url));
+        var exception = Assert.ThrowsAsync<Exception>(async () =>
+        await _service.GetRecipeFromUrl(url));
 
-//         Assert.That(exception.Message, Does.Contain("invalid recipe"));
-//     }
-// }
+        Assert.That(exception.Message, Does.Contain("invalid recipe"));
+    }
+}
