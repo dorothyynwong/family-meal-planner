5f07d6a7004bd041d68ffb0e4985f8e7d9e4f613
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-11-02 16:50:23 +0000

    familymealform can search recipes

--- frontend/src/Components/FamilyMealForm/FamilyMealForm.tsx
+++ frontend/src/Components/FamilyMealForm/FamilyMealForm.tsx
@@ -1,12 +1,15 @@
-import { Form } from "react-bootstrap";
+import { Button, Form, InputGroup } from "react-bootstrap";
 import MealFormBase from "../MealFormBase/MealFormBase";
 import { useMeal } from "../MealContext/MealContext";
 import { Dayjs } from 'dayjs';
 import StatusHandler from "../StatusHandler/StatusHandler";
 import Popup from "../Popup/Popup";
 import useMealForm from "../../Hooks/useMealForm";
-import RecipeSearch from "../RecipeSearch/RecipeSearch";
+// import RecipeSearch from "../RecipeSearch/RecipeSearch";
 import { useNavigate } from "react-router-dom";
+// import SearchBar from "../SearchBar/SearchBar";
+import { FaSearch } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 interface FamilyMealFormProps {
     isForFamily?: boolean
@@ -22,16 +25,24 @@ const FamilyMealForm: React.FC<FamilyMealFormProps> = ({ isForFamily, selectedDa
         mode,
         status,
         errorMessages,
+        // setRecipeName,
         recipeName,
-        mealDate,
-        selectedMealType,
     } = useMeal();
 
     const navigate = useNavigate();
 
-    const isFromMealForm = true;
+    const { handleSubmit } = useMealForm(isForFamily, selectedDate);
+    const [searchValue, setSearchValue] = useState("");
 
-    const { handleSubmit} = useMealForm(isForFamily, selectedDate);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target.value);
+    }
+
+    useEffect(() => {
+        setSearchValue(recipeName);
+    }, [recipeName])
+
+    const isFromMealForm = true;
 
     return (
         <Popup
@@ -48,7 +59,21 @@ const FamilyMealForm: React.FC<FamilyMealFormProps> = ({ isForFamily, selectedDa
                         readOnly
                         placeholder={selectedFamily?.familyName} />
                 </Form.Group>
-                <RecipeSearch recipeName={recipeName} onSearchClick={() => navigate(`/recipes-list`, { state: { isFromMealForm, mealDate, selectedMealType } })} />
+                <InputGroup className="mt-3 recipe-search-container">
+                    <InputGroup.Text className="recipe-search-icon-box">
+                        <FaSearch className="recipe-search-icon" />
+                    </InputGroup.Text>
+                    <Form.Control
+                        className="recipe-search-box"
+                        placeholder="Search Recipe"
+                        aria-label="Search"
+                        onChange={handleChange}
+                        value={searchValue}
+                    />
+                    <Button className="custom-button" onClick={() => navigate(`/recipes-search/${searchValue}`, { state: { isFromMealForm } })}>
+                        Search
+                    </Button>
+                </InputGroup>
                 <MealFormBase isForFamily={isForFamily} selectedDate={selectedDate} />
                 <StatusHandler
                     status={status}
--- frontend/src/Pages/RecipesSearchResult/RecipesSearchResult.tsx
+++ frontend/src/Pages/RecipesSearchResult/RecipesSearchResult.tsx
@@ -1,4 +1,4 @@
-import { useLocation} from "react-router-dom";
+import { useLocation, useParams} from "react-router-dom";
 import RecipeCard from "../../Components/RecipeCard/RecipeCard";
 import { Row } from "react-bootstrap";
 import { InfiniteList } from "../../Components/InfiniteList/InfiniteList";
@@ -6,11 +6,15 @@ import { searchRecipes } from "../../Api/api";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import { useState } from "react";
 import useDebounce from "../../Hooks/useDebounce";
+// import { useMeal } from "../../Components/MealContext/MealContext";
 
 const RecipesSearchResult: React.FC = () => {
     const location = useLocation();
     const isFromMealForm = location.state?.isFromMealForm || false;
-    const [searchValue, setSearchValue] = useState("");
+    // const {recipeName} = useMeal();
+    const {searchRecipeName} = useParams<{ searchRecipeName?: string }>();
+    
+    const [searchValue, setSearchValue] = useState(searchRecipeName || "");
 
     const debouncedSearchValue = useDebounce(searchValue, 1000);
 
