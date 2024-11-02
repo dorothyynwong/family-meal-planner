7d8628cfcb1a57c26bf8d45ce0cd3bbffb9a2c4f
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-11-02 15:09:14 +0000

    refactor

--- frontend/src/Hooks/useMealForm.tsx
+++ frontend/src/Hooks/useMealForm.tsx
@@ -124,7 +124,6 @@ function useMealForm(isForFamily?: boolean, selectedDate?: Dayjs) {
                 });
         }
         else {
-            console.log(meal);
             updateMeal(meal, currentMeal!.id!)
                 .then(response => {
                     if (response.status === 200) {
