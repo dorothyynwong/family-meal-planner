910c9f0b753c60d860a0718c2a66d3ed8f1784bf
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-11-04 13:44:14 +0000

    error handling in userupdatepage

--- frontend/src/Pages/UserUpdatePage/UserUpdatePage.tsx
+++ frontend/src/Pages/UserUpdatePage/UserUpdatePage.tsx
@@ -2,19 +2,40 @@ import { useEffect, useState } from "react";
 import UserForm from "../../Components/UserForm/UserForm";
 import { UserSignupInterface } from "../../Api/apiInterface";
 import { getUser } from "../../Api/api";
+import StatusHandler from "../../Components/StatusHandler/StatusHandler";
 
 const UserUpdatePage: React.FC = () => {
+    const [status, setStatus] = useState<"idle" | "loading" | "success" | "error">("idle");
+    const [errorMessages, setErrorMessages] = useState<string[]>([]);
+
     const [userData, setUserData] = useState<UserSignupInterface>({});
-    
+
     useEffect(() => {
+        setStatus("loading");
         getUser()
-        .then(response => setUserData(response.data))
-        .catch(error => console.log(error));
+            .then(response => {
+                setUserData(response.data);
+                setStatus("success");
+            })
+            .catch(error => {
+                console.log("Error login", error);
+                const errorMessage = error?.response?.data?.message || "Error getting user details";
+                setStatus("error");
+                setErrorMessages([...errorMessages, errorMessage]);
+            });
     }, []);
 
     return (
         <>
-            <UserForm data={userData} mode="edit"/>
+            <StatusHandler
+                status={status}
+                errorMessages={errorMessages}
+                loadingMessage="Getting user details..."
+                successMessage=""
+            >
+                <></>
+            </StatusHandler>
+            <UserForm data={userData} mode="edit" />
         </>
     )
 }
