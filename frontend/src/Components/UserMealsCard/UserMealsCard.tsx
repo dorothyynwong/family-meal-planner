09529904bd29786e743b7b2d532c3e9e996a90d6
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-11-04 14:05:48 +0000

    changed usermealscard avatar

--- frontend/src/Components/UserMealsCard/UserMealsCard.tsx
+++ frontend/src/Components/UserMealsCard/UserMealsCard.tsx
@@ -1,9 +1,11 @@
-import { Avatar, Card, CardActions, CardContent, CardHeader, Collapse, IconButton, IconButtonProps, styled } from "@mui/material";
+import { Card, CardActions, CardContent, CardHeader, Collapse, IconButton, IconButtonProps, styled } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState } from "react";
 import { red } from "@mui/material/colors";
 import MealDaily from "../MealDaily/MealDaily";
 import { FamilyUserInterface } from "../../Api/apiInterface";
+import Avatar from "react-avatar";
+import { useAuth } from "../AuthProvider/AuthProvider";
 
 interface ExpandMoreProps extends IconButtonProps {
     expand: boolean;
@@ -40,6 +42,8 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
 }));
 
 const UserMealsCard: React.FC<UserMealsProps> = ({mealDate, data}) => {
+    const { nickname, avatarColor, avatarFgColor } = useAuth();
+    
     const [expanded, setExpanded] = useState(false);
     
     const handleExpandClick = () => {
@@ -51,9 +55,11 @@ const UserMealsCard: React.FC<UserMealsProps> = ({mealDate, data}) => {
             <Card sx={{ maxWidth: 345, mx:0, mb:1, p:0.5 }}>
                 <CardHeader
                     avatar={
-                        <Avatar sx={{ bgcolor: red[500] }} aria-label="user avator">
-                            {data.userNickName}
-                        </Avatar>
+                        <Avatar  name={nickname} 
+                                    color={avatarColor != "" ? avatarColor : "#796C50" } 
+                                    fgColor={avatarFgColor} 
+                                    size="50" 
+                                    round={true}/>
                     }
                     title={data.userNickName}
                     sx={{p:0, mx:0 }}
