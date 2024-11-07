0945b5f1d65ba8270272d174b320e0f5cdeed794
dorothyynwong <dorothywyn@users.noreply.github.com>
2024-11-07 17:23:24 +0000

    setup vitest and test for Home

---
+++ frontend/src/__test__/Home.test.tsx
@@ -0,0 +1,15 @@
+// Imports
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Home from '../Pages/Home/Home';
+
+describe('Renders home page correctly', async () => {
+    it('Should render the page correctly', async () => {
+        // Setup
+        render(<Home />);
+        const h1 = await screen.queryByText('Today\'s Feast');
+
+        // Expectations
+        expect(h1).not.toBeNull();
+    });
+});
